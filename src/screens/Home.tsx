9a8f7ea6c07e388a24efad123456292480d34a02
PauloDaude <paulodaude14@gmail.com>
2024-04-20 10:21:45 -0300

    build: configure tailwind css

---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View className="flex-1 bg-black justify-center items-center">
+      <Text className="text-lg font-bold text-white">Home</Text>
+    </View>
+  );
+};
+export default Home;
