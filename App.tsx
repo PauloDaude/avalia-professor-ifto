030b66fdea8c77c689c30f7a440207548eccf70c
PauloDaude <paulodaude14@gmail.com>
2024-04-26 14:02:58 -0300

    refactor: change folder font name to styles

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import Home from './src/screens/Home';
-import { loadFonts } from './src/font/fontCustom';
+import { loadFonts } from './src/styles/fontCustom';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
