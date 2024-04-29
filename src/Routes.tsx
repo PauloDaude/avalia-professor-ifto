6bcbb6dc69bdba919152979700fb8de9fbc355cd
PauloDaude <paulodaude14@gmail.com>
2024-04-29 16:38:04 -0300

    feat: add type to routes for navigations

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,16 @@
 import Home from '../src/screens/Home';
 import Login from '../src/screens/Login';
+import Courses from '../src/screens/Courses';
 import { loadFonts } from '../src/styles/fontCustom';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Home: undefined;
+  Courses: undefined;
+  Login: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Routes() {
   const [fontsLoaded] = loadFonts();
@@ -15,8 +22,9 @@ export default function Routes() {
       initialRouteName="Home"
       screenOptions={{ headerShown: false }}
     >
-      <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Courses" component={Courses} />
     </Stack.Navigator>
   );
 }
