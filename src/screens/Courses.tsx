d329fd1a82cf2bf4747a8f56c1d14c52847244ef
PauloDaude <paulodaude14@gmail.com>
2024-05-08 14:27:36 -0300

    refactor: use back FlatList instead of DataList component

--- src/screens/Courses.tsx
+++ src/screens/Courses.tsx
@@ -1,17 +1,26 @@
-import { View } from 'react-native';
-import useDataList from '../hooks/useDataList';
-import Header from '../components/Header';
 // import FocusedStatusBar from '../components/FocusedStatusBar';
-import { StatusBar } from 'react-native';
+import { FlatList, View, StatusBar } from 'react-native';
+
+import Header from '../components/Header';
 import Title from '../components/Title';
 import Subtitle from '../components/Subtitle';
 import Separator from '../components/Separator';
-import DataList from '../components/DataList';
+import Loading from '../components/Loading';
+import ItemList from '../components/ItemList';
+
+import { RoutesParams } from '../Routes';
+import useDataList, { ICourses } from '../hooks/useDataList';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 
 const Courses = () => {
   const baseURL = 'http://192.168.0.195:3000';
 
   const { data, loading, fetchData } = useDataList(baseURL);
+
+  const navigation = useNavigation<NavigationProp<RoutesParams>>();
+  const handleItemPress = (data: ICourses) => {
+    navigation.navigate('Classes', { data });
+  };
 
   return (
     <>
@@ -23,7 +32,21 @@ const Courses = () => {
           <Subtitle>Qual curso você estuda?</Subtitle>
         </View>
         <Separator text="Nível Superior" />
-        <DataList data={data} loading={loading} loadMore={fetchData} />
+        <View className="flex-1 px-6">
+          <FlatList
+            data={data}
+            keyExtractor={item => String(item.id)}
+            renderItem={({ item }) => (
+              <ItemList
+                text={item.name}
+                onPress={() => handleItemPress(item)}
+              />
+            )}
+            onEndReached={fetchData}
+            onEndReachedThreshold={0.1}
+            ListFooterComponent={loading ? <Loading /> : null}
+          />
+        </View>
       </View>
     </>
   );
