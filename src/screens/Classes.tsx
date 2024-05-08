3263a7f73bf39736824cdb46ac9080f70f3b26c8
PauloDaude <paulodaude14@gmail.com>
2024-05-08 14:30:34 -0300

    feat: add dynamically classes with separate periods

--- src/screens/Classes.tsx
+++ src/screens/Classes.tsx
@@ -1,32 +1,92 @@
-import { View, Text, StatusBar } from 'react-native';
+import { View, Text, StatusBar, FlatList } from 'react-native';
+
+import { ScrollView } from 'react-native-virtualized-view';
+
 import Header from '../components/Header';
 import Title from '../components/Title';
 import Subtitle from '../components/Subtitle';
 import Separator from '../components/Separator';
-import DataList from '../components/DataList';
-import { RouteProp } from '@react-navigation/native';
-import { RootStackParamList } from '../Routes';
 
-type ClassesRouteProp = RouteProp<RootStackParamList, 'Classes'>;
+import { RoutesParams } from '../Routes';
+import { RouteProp } from '@react-navigation/native';
+import { IClass, ICourses } from '../hooks/useDataList';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import ItemList from '../components/ItemList';
+
+type ClassesRouteProp = RouteProp<RoutesParams, 'Classes'>;
 
 interface ClassesProps {
   route: ClassesRouteProp;
 }
 
 const Classes = ({ route }: ClassesProps) => {
-  const { itemName } = route.params;
+  const navigation = useNavigation<NavigationProp<RoutesParams>>();
+  const handleItemPress = (data: ICourses) => {
+    navigation.navigate('Classes', { data });
+  };
+
+  const { data } = route.params;
+  const periodsList: IClass[][] = Object.values(data.classes);
+  // console.log(Object.values(data.classes));
+
+  const isVoidData = () => {
+    const teste = Object.values(data.classes).filter(test =>
+      test.length !== 0 ? false : true
+    );
+    if (teste.length === 0) {
+      return true;
+    }
+    return false;
+  };
 
   return (
     <>
       <StatusBar backgroundColor="#257C2E" />
       <View className="flex-1 bg-white">
-        <Header title="Matérias do curso" subTitle={itemName} showBack={true} />
-        <View className="p-6">
+        <Header
+          title="Matérias do curso"
+          subTitle={data.name}
+          showBack={true}
+        />
+        <View className="p-6 shadow-md">
           <Title>Escolha a disciplina</Title>
           <Subtitle>Estão separadas por período</Subtitle>
         </View>
-        <Separator text="Nível Superior" />
-        {/* <DataList data={data} loading={loading} loadMore={loadMore} /> */}
+        <ScrollView>
+          <View className="flex-1">
+            {isVoidData() && (
+              <Text className="py-3 px-6 bg-[#EEEFF1] text-sm font-OpenSansRegular text-secondary-green">
+                Nenhuma matéria encontrada
+              </Text>
+            )}
+            {periodsList.map((period: IClass[], index: number) => (
+              <View key={index}>
+                {period.length > 0 && (
+                  <>
+                    <Separator
+                      text={`${
+                        (index + 1) % 2 == 0 ? index + 2 : index + 1
+                      }º Período`}
+                    />
+                    <View className="px-6">
+                      <FlatList
+                        data={period}
+                        keyExtractor={item => String(item.id)}
+                        renderItem={({ item }) => (
+                          <ItemList
+                            text={item.class}
+                            subText={item.professor}
+                            // onPress={() => handleItemPress(item)}
+                          />
+                        )}
+                      />
+                    </View>
+                  </>
+                )}
+              </View>
+            ))}
+          </View>
+        </ScrollView>
       </View>
     </>
   );
