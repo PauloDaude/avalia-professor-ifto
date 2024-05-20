c7717b9e7d846e10594c59081ac96cd678663ca2
PauloDaude <paulodaude14@gmail.com>
2024-05-20 16:18:01 -0300

    feat: add disabled prop in Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,14 +9,20 @@ import { FontAwesome6 } from '@expo/vector-icons';
 interface IButton extends TouchableOpacityProps {
   text: string;
   variant?: 'login' | undefined;
+  disabled?: boolean;
 }
 
-const Button = ({ variant, text, ...rest }: IButton) => {
-  let bgColor = variant ? 'bg-[#4285F4]' : 'bg-primary-green';
+const Button = ({ variant, text, disabled, ...rest }: IButton) => {
+  let bgColor = disabled
+    ? 'bg-[#257c2e55]'
+    : variant
+    ? 'bg-[#4285F4]'
+    : 'bg-primary-green';
   return (
     <TouchableOpacity
       {...rest}
       activeOpacity={0.8}
+      disabled={disabled}
       className={`w-full ${bgColor} rounded-lg items-center justify-center`}
     >
       <View className="flex-row w-full items-center justify-center p-4 gap-4">
