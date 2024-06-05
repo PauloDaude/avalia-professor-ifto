b164b34b6cac849a366920f027214733c09d5233
PauloDaude <paulodaude14@gmail.com>
2024-06-04 22:23:13 -0300

    feat: add professors nterfaces

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -21,6 +21,24 @@ export interface IFormData {
   Nota6: number;
 }
 
+export interface IAverageNotes {
+  name: string;
+  media_nota1: number;
+  media_nota2: number;
+  media_nota3: number;
+  media_nota4: number;
+  media_nota5: number;
+  media_nota6: number;
+  media_total: number;
+}
+
+export interface IProfessorNotes {
+  id_professor: number;
+  professor: string;
+  media_por_materia: IAverageNotes;
+  media_das_medias: number;
+}
+
 export interface INote {
   [key: number]: number;
 }
