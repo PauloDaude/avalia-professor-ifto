b4e4570f19a39556e6002fa74b428ba801e941b0
PauloDaude <paulodaude14@gmail.com>
2024-06-04 22:24:47 -0300

    refactor: rename file name, from Courses to HomeStudent

--- src/screens/HomeStudent.tsx
+++ src/screens/HomeStudent.tsx
@@ -1,5 +1,10 @@
 // import FocusedStatusBar from '../components/FocusedStatusBar';
 import { FlatList, View, StatusBar } from 'react-native';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { useQuery } from '@tanstack/react-query';
+import { RoutesParams } from '../Routes';
+import { IClassesScreen } from '../interfaces/screens';
+import axios from 'axios';
 
 import Header from '../components/Header';
 import Title from '../components/Title';
@@ -7,12 +12,6 @@ import Subtitle from '../components/Subtitle';
 import Separator from '../components/Separator';
 import Loading from '../components/Loading';
 import ItemList from '../components/ItemList';
-
-import { RoutesParams } from '../Routes';
-import { IClassesScreen } from '../interfaces/screens';
-import { NavigationProp, useNavigation } from '@react-navigation/native';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 
 const Courses = () => {
   const baseURL = 'https://felipeoliveira.pythonanywhere.com/api/cursos';
@@ -31,7 +30,11 @@ const Courses = () => {
     <>
       <StatusBar backgroundColor="#257C2E" />
       <View className="flex-1 bg-white">
-        <Header title="Paulo Daúde" subTitle="Aluno" showAvatar={true} />
+        <Header
+          title="Paulo Daúde"
+          subTitle="Aluno"
+          avatarURL="https://i.ibb.co/XWJ1ML0/unnamed.jpg"
+        />
         <View className="p-6">
           <Title>Bem vindo, Paulo!</Title>
           <Subtitle>Qual curso você estuda?</Subtitle>
